a7f47d27570ca846cfec51ef79573f57442ed10b
Greg Albrecht <gba@onbeep.com>
2017-08-28 13:01:33 -0700

    moving out of beta

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import setuptools
 import sys
 
 __title__ = 'kiss'
-__version__ = '7.0.0b3'
+__version__ = '7.0.0'
 __author__ = 'Greg Albrecht W2GMD <oss@undef.net>'  # NOQA pylint: disable=R0801
 __copyright__ = 'Copyright 2017 Greg Albrecht and Contributors'  # NOQA pylint: disable=R0801
 __license__ = 'Apache License, Version 2.0'  # NOQA pylint: disable=R0801
